9e2496f8f816696cf82bba06b51d554dbe9a6381
Assanfakou <fakouhasan@gmail.com>
2025-09-23 20:24:14 +0100

    ADD config

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -17,47 +17,98 @@ class Contact
 			p_Number = "";
 			darkest_Secret = "";
 		}
-		void set_f_name(std::string f_name)
+		void set_f_name(void)
 		{
-			this->f_Name = f_name;
+			std::string line;
+			while (line.empty())
+			{
+				std::cout <<"First Name :";
+				std::getline(std::cin, line);
+				if (line.empty())
+					std::cout <<"please enter your First Name\n";
+			}
+			this->f_Name = line;
 		}
-		void set_l_name(std::string l_name)
+		void set_l_name(void)
 		{
-			this->l_Name = l_name;
+			std::string line;
+
+			while (line.empty())
+			{
+				std::cout <<"Last Name :";
+				std::getline(std::cin, line);
+				if (line.empty())
+					std::cout <<"please enter your Last Name\n";
+			}
+			this->l_Name = line;
 		}
-		void set_nickname(std::string nickname)
+		void set_nickname(void)
 		{
-			this->nickname = nickname;
+			std::string line;
+
+			while (line.empty())
+			{
+				std::cout <<"Nickname :";
+				std::getline(std::cin, line);
+				if (line.empty())
+					std::cout <<"please enter your nickname\n";
+			}
+			this->nickname = line;
 		}
-		void set_p_number(std::string p_number)
+		void set_p_number(void)
 		{
-			this->p_Number = p_number;
+			std::string line;
+
+			while (line.empty())
+			{
+				std::cout <<"Phone Number :";
+				std::getline(std::cin, line);
+				if (line.empty())
+					std::cout <<"please enter your Phone Number\n";
+			}
+			this->p_Number = line;
 		}
-		void set_darkest_secret(std::string darkSecret)
+		void set_darkest_secret(void)
 		{
-			this->darkest_Secret = darkSecret;
+			std::string line;
+
+			while (line.empty())
+			{
+				std::cout <<"Darkes Secret :";
+				std::getline(std::cin, line);
+				if (line.empty())
+					std::cout <<"please enter your Darkest Secret\n";
+			}
+			this->darkest_Secret = line;
 		}
 		void print()
 		{
-			std::cout << f_Name << " "<< std::endl;
+			std::cout << f_Name << std::endl;
+			std::cout << l_Name << std::endl;
+			std::cout << nickname << std::endl;
+			std::cout << p_Number << std::endl;
+			std::cout << darkest_Secret << std::endl;
 		}
 };
 
 int main ()
 {
-	std::string comand;
+	std::string command;
+	std::string line;
 	Contact con;
 
 	while (1)
 	{
 		std::cout << "ADD || SEARCH || EXIT :";
-		std::getline(std::cin, line);
-		while (line.compare("ADD") == 0)
+		std::getline(std::cin, command);
+		if (command.compare("ADD") == 0)
 		{
-			std::cout <<"First Name :";
-			std::getline(std::cin, line);
+			con.set_f_name();
+			con.set_l_name();
+			con.set_nickname();
+			con.set_p_number();
+			con.set_darkest_secret();
 		}
-		std::cout << line << std::endl;
+		con.print();
 	}
-	con.print();
 }
