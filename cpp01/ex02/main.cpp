a96a11399c933db08cf80e08abd3c3aaed256dc2
Assanfakou <fakouhasan@gmail.com>
2025-10-16 12:13:41 +0100

    exo2 defference between reference and pointers done

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main ()
+{
+	std::string var = "HI THIS IS BRAIN";
+	std::string *stringPTR = &var;
+	std::string& stringREF = var;
+
+	std::cout << "The memory address of var : " << &var << std::endl;
+	std::cout << "The memory address that stringPTR holds : " << stringPTR << std::endl;
+	std::cout << "The memory address that stringREF holds : " << &stringREF << std::endl;
+
+	std::cout << "The value of var : " << var << std::endl;
+	std::cout << "The value stringPTR points to : " << *stringPTR << std::endl;
+	std::cout << "The value stringREF points to : " << stringREF << std::endl;
+} 
