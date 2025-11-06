2e8d3060b2306cbf25d017a29ca22a2b25e3bb5e
Assanfakou <fakouhasan@gmail.com>
2025-11-06 17:09:45 +0100

    nothing changed ?

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,39 @@
+#include "Harl.hpp"
+
+void Harl::debug()
+{
+	std::cout << Green <<  "[DEBUG]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << Yellow << "[INFO]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t putenough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+	std::cout << Blue << "[WARNING]" << std::endl;
+	std::cout << "think I deserve to have some extra bacon for free. I’ve been coming foryears whereas you started working here since last month." << std::endl;
+}
+void Harl::error()
+{
+	std::cout << Redr << "[ERROR]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+	void (Harl::*functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string level1[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	for (int i = 0; i < 4;i++)
+	{
+		if (level1[i] == level)
+		{
+			(this->*functions[i])();
+			return ;
+		}
+	}
+}
---
+++ cpp01/ex05/Harl.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#define Black "\033[0;30m"        // Black
+#define Redr "\033[0;31m"         // Red
+#define Green "\033[0;32m"        // Green
+#define Yellow "\033[0;33m"       // Yellow
+#define Blue "\033[0;34m"         // Blue
+#define Purple "\033[0;35m"       // Purple
+#define Cyan "\033[0;36m"         // Cyan
+#define White "\033[0;37m"        // White
+
+class Harl
+{
+	private:
+	void debug();
+	void info();
+	void warning();
+	void error();
+	public:
+	void complain(std::string level);
+};
+
+#endif
