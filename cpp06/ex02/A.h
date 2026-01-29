4ef212bda44b0117bd077bc356f89947c44f059d
Assanfakou <fakouhasan@gmail.com>
2026-01-29 11:51:35 +0100

    casting dynamically and staticly

---
+++ cpp06/ex02/A.h
@@ -0,0 +1,10 @@
+#ifndef A_HPP
+#define A_HPP
+
+#include "Base.h"
+
+class A : public Base {
+};
+
+
+#endif
---
+++ cpp06/ex02/B.h
@@ -0,0 +1,9 @@
+#ifndef B_HPP
+#define B_HPP
+
+#include "Base.h"
+
+class B : public Base {
+};
+
+#endif
