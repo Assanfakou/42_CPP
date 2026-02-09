086098d4d45433e50b578b25c6d1472218e2bfcb
Assanfakou <fakouhasan@gmail.com>
2026-02-09 18:15:31 +0100

    readme

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,15 @@ int main ()
     {
         Array<int> a(3);
         Array<char> b(4);
+        const Array<char> c(3);
+
 
         a[0] = 366;
         b[0] = 97;
         std::cout << a[0] << "\n";
         std::cout << b[0] << "\n";
-        std::cout << b[6] << "\n";
+        std::cout << b[2] << "\n";
+        std::cout << c[2] << "\n";
     }
     catch (std::exception &e)
     {
