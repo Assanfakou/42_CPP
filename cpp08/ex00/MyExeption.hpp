a80823e49244a7df251213c5a3bead68ed9f1c39
Assanfakou <fakouhasan@gmail.com>
2026-04-10 12:47:51 -0400

    the template in the header

--- cpp08/ex00/MyExeption.hpp
+++ cpp08/ex00/MyExeption.hpp
@@ -3,8 +3,20 @@
 
 #include <iostream>
 
+
 class MyExeption :public std::exception
 {
     public:
         const char *what() const throw();
 };
+
+template <typename T>
+
+typename T::iterator easyfind(T& container, int value)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+
+    if (it == container.end())
+        throw MyExeption();
+    return it;
+}
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,17 +3,6 @@
 #include <algorithm>
 #include "MyExeption.hpp"
 
-template <typename T>
-
-typename T::iterator easyfind(T& container, int value)
-{
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-
-    if (it == container.end())
-        throw MyExeption();
-    return it;
-}
-
 int main ()
 {
     std::vector<int> v = {1, 3, 4, 4, 5, 5, 6, 4};
