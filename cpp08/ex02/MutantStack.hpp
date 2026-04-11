cc9e405d86d61abe41bdaccc8ff9273e89f7b160
Assanfakou <fakouhasan@gmail.com>
2026-04-11 07:30:51 -0400

    mutedstack bye

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,34 @@
+#ifndef MUTANT_STACK
+#define MUTANT_STACK
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+template <typename T>
+
+class MutantStack : public std::stack<T>
+{
+  public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin()
+    {
+      return this->c.begin();
+    }
+    iterator end()
+    {
+      return this->c.end();
+    }
+    MutantStack() {}
+    MutantStack(const MutantStack &other) : std::stack<T>(other){}
+    MutantStack &operator=(const MutantStack &other)
+    {
+      std::stack<T>::operator=(other);
+      return *this;
+    }
+    ~MutantStack() {}
+};
+
+#endif
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <list>
+#include "MutantStack.hpp"
+
+int main()
+{
+    std::cout << "============================" << std::endl;
+    std::cout << "TEST 1: MutantStack" << std::endl;
+    std::cout << "============================" << std::endl;
+
+    MutantStack<int> mstack;
+
+    mstack.push(10);
+    mstack.push(20);
+    mstack.push(30);
+    mstack.push(40);
+
+    std::cout << "Top (stack behavior): " << mstack.top() << std::endl;
+
+    std::cout << "Iteration (container behavior):" << std::endl;
+    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+        std::cout << *it << std::endl;
+
+
+    std::cout << "\n============================" << std::endl;
+    std::cout << "TEST 2: std::list (reference)" << std::endl;
+    std::cout << "============================" << std::endl;
+
+    std::list<int> lst;
+
+    lst.push_back(10);
+    lst.push_back(20);
+    lst.push_back(30);
+    lst.push_back(40);
+
+    std::cout << "Iteration (list behavior):" << std::endl;
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+        std::cout << *it << std::endl;
+
+    std::cout << "\n============================" << std::endl;
+    std::cout << "TEST 3: consistency check" << std::endl;
+    std::cout << "============================" << std::endl;
+
+    std::cout << "Both should print same sequence: 10 20 30 40" << std::endl;
+
+    std::cout << "\nMutantStack iteration:" << std::endl;
+    for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+        std::cout << *it << " ";
+
+    std::cout << "\nList iteration:" << std::endl;
+    for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+        std::cout << *it << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
