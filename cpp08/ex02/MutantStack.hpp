03ee28c9114559596961c18f61e099156dfdd7ad
Assanfakou <fakouhasan@gmail.com>
2026-05-14 19:59:57 -0400

    structure de synthax

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -10,25 +10,25 @@ template <typename T>
 
 class MutantStack : public std::stack<T>
 {
-  public:
-    typedef typename std::stack<T>::container_type::iterator iterator;
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
 
-    iterator begin()
-    {
-      return this->c.begin();
-    }
-    iterator end()
-    {
-      return this->c.end();
-    }
-    MutantStack() {}
-    MutantStack(const MutantStack &other) : std::stack<T>(other){}
-    MutantStack &operator=(const MutantStack &other)
-    {
-      std::stack<T>::operator=(other);
-      return *this;
-    }
-    ~MutantStack() {}
+		iterator begin()
+		{
+			return this->c.begin();
+		}
+		iterator end()
+		{
+			return this->c.end();
+		}
+		MutantStack() {}
+		MutantStack(const MutantStack &other) : std::stack<T>(other){}
+		MutantStack &operator=(const MutantStack &other)
+		{
+			std::stack<T>::operator=(other);
+			return *this;
+		}
+		~MutantStack() {}
 };
 
 #endif
