ea7daa5783648b1b23bb7e8bb195e681f7bd075b
Assanfakou <fakouhasan@gmail.com>
2026-05-15 07:54:10 -0400

    check valide date

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -32,7 +32,32 @@ void BitcoinExchange::print_map()
 		std::cout << it->first << " " << it->second << std::endl;
 	}
 }
-
+bool valid_date(const std::string& date)
+{
+	if (date.size() != 10)
+		return false;
+	if (date[4] != '-' || date[7] != '-')
+		return false;
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 4 || i == 7)
+			continue;
+		if (!std::isdigit(date[i]))
+			return false;
+	}
+	int year = std::atoi(date.substr(0, 4).c_str());
+	int month = std::atoi(date.substr(5, 2).c_str());
+	int day = std::atoi(date.substr(8, 2).c_str());
+	
+	if (year < 0)
+		return false;
+	if (month < 1 || month > 12)
+		return false;
+	if (day < 1 || day > 31)
+		return false
+	return true;
+}
+		
 void BitcoinExchange::loadDatabase(const std::string &filename)
 {
 	std::ifstream datafile(filename.c_str());
@@ -50,6 +75,8 @@ void BitcoinExchange::loadDatabase(const std::string &filename)
 		if (comma == std::string::npos)
 			continue;
 		date = line.substr(0, comma);
+		if (!check_date(date))
+			throw TimeFormat(); 
 		value = line.substr(comma + 1);
 
 		data[date] = atof(value.c_str());
