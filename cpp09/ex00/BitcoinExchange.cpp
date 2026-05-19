59bf7663e6f40b634e0cae3c82321ae6f58d8a28
Assanfakou <fakouhasan@gmail.com>
2026-05-19 07:27:46 -0400

    parce time of day 31 on every mounth check and also checke \n the month 2 if it's a leap year it can get to 29 other wise no

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -52,7 +52,21 @@ bool BitcoinExchange::check_date(const std::string& date)
 		return false;
 	if (month < 1 || month > 12)
 		return false;
-	if (day < 1 || day > 31)
+	if (day >= 1)
+	{
+		if (day > 31)
+			return false;
+		if (day == 31 && (month == 4 || month == 6 || month == 9 || month == 11))	
+			return false;
+		if (month == 2)
+		{
+			if (day > 29)
+				return false;
+			if ((year % 100) % 4 != 0 && day == 29)
+				return false;
+		}
+	}
+	else
 		return false;
 	return true;
 }
